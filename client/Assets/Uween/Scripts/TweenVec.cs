dcbacd02db4d006d7083a324cb919208b8157c75
Yoshihiro Shindo <beinteractive.org@gmail.com>
2014-12-03 13:09:12 +0900

    リファクタリング

---
+++ client/Assets/Uween/Scripts/TweenVec.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class TweenVec<V> : Tween
+{
+    protected static T Add<T, TV>(GameObject g, float duration, TV to) where T : TweenVec<TV>
+    {
+        var t = Tween.Get<T>(g, duration);
+        t.to = to;
+        return t;
+    }
+    
+    public V from;
+    public V to;
+    
+    public abstract Vector3 vector { get; set; }
+    public abstract V value { get; set; }
+    
+    Transform t;
+    
+    protected Transform GetTransform()
+    {
+        if (t == null) {
+            t = transform;
+        }
+        return t;
+    }
+    
+    override protected void Reset()
+    {
+        base.Reset();
+        from = value;
+        to = value;
+    }
+}
+
+public static class TweenVecExtensions
+{
+    public static T From<T, V>(this T tween, V from) where T : TweenVec<V>
+    {
+        tween.from = from;
+        tween.value = from;
+        return tween;
+    }
+}
--- client/Assets/Uween/Scripts/TweenVec1.cs
+++ client/Assets/Uween/Scripts/TweenVec1.cs
@@ -1,50 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public abstract class TweenVec1 : Tween
+public abstract class TweenVec1 : TweenVec<float>
 {
     protected static T Add<T>(GameObject g, float duration, float to) where T : TweenVec1
     {
-        var t = Tween.Get<T>(g, duration);
-        t.to = to;
-        return t;
+        return Add<T, float>(g, duration, to);
     }
 
-    public float from;
-    public float to;
-
-    public abstract Vector3 vector { get; set; }
-    public abstract float value { get; set; }
-
-    Transform t;
-
-    protected Transform GetTransform()
-    {
-        if (t == null) {
-            t = transform;
-        }
-        return t;
-    }
-    
-    override protected void Reset()
-    {
-        base.Reset();
-        from = value;
-        to = value;
-    }
-    
     override protected void UpdateValue(float f)
     {
         value = from + (to - from) * f;
-    }
-}
-
-public static class TweenVec1Extensions
-{
-    public static T From<T>(this T tween, float from) where T : TweenVec1
-    {
-        tween.from = from;
-        tween.value = from;
-        return tween;
     }
 }
