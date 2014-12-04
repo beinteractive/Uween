5bd4ca9f35e0e03cad7a706d89f8f1f994f0506e
Yoshihiro Shindo <beinteractive.org@gmail.com>
2014-12-04 10:59:56 +0900

    リファクタリング

--- client/Assets/Uween/Scripts/TweenVec1T.cs
+++ client/Assets/Uween/Scripts/TweenVec1T.cs
@@ -15,3 +15,48 @@ public abstract class TweenVec1T<T> : TweenVec1<T>
         return t;
     }
 }
+
+public abstract class TweenVec1P<T> : TweenVec1T<T>
+{
+    override public Vector3 vector
+    {
+        get
+        {
+            return GetTransform().localPosition;
+        }
+        set
+        {
+            GetTransform().localPosition = value;
+        }
+    }
+}
+
+public abstract class TweenVec1R<T> : TweenVec1T<T>
+{
+    override public Vector3 vector
+    {
+        get
+        {
+            return GetTransform().localRotation.eulerAngles;
+        }
+        set
+        {
+            GetTransform().localRotation = Quaternion.Euler(value);
+        }
+    }
+}
+
+public abstract class TweenVec1S<T> : TweenVec1T<T>
+{
+    override public Vector3 vector
+    {
+        get
+        {
+            return GetTransform().localScale;
+        }
+        set
+        {
+            GetTransform().localScale = value;
+        }
+    }
+}
--- client/Assets/Uween/Scripts/TweenVec2T.cs
+++ client/Assets/Uween/Scripts/TweenVec2T.cs
@@ -15,3 +15,48 @@ public abstract class TweenVec2T<T> : TweenVec2<T>
         return t;
     }
 }
+
+public abstract class TweenVec2P<T> : TweenVec2T<T>
+{
+    override public Vector3 vector
+    {
+        get
+        {
+            return GetTransform().localPosition;
+        }
+        set
+        {
+            GetTransform().localPosition = value;
+        }
+    }
+}
+
+public abstract class TweenVec2R<T> : TweenVec2T<T>
+{
+    override public Vector3 vector
+    {
+        get
+        {
+            return GetTransform().localRotation.eulerAngles;
+        }
+        set
+        {
+            GetTransform().localRotation = Quaternion.Euler(value);
+        }
+    }
+}
+
+public abstract class TweenVec2S<T> : TweenVec2T<T>
+{
+    override public Vector3 vector
+    {
+        get
+        {
+            return GetTransform().localScale;
+        }
+        set
+        {
+            GetTransform().localScale = value;
+        }
+    }
+}
