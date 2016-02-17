3625f0fa9d90f4e11c771f8b4374900b2ead29e3
Yoshihiro Shindo <beinteractive.org@gmail.com>
2016-02-17 18:17:27 +0900

    Preview Settings

--- Scripts/Preview.cs
+++ Scripts/Preview.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Uween
 {
 	public class Preview : MonoBehaviour
 	{
+		[SerializeField]
+		public List<PreviewSetting> settings = new List<PreviewSetting>();
 	}
 }
---
+++ Scripts/PreviewSetting.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Uween
+{
+	public class PreviewSetting : ScriptableObject
+	{
+		public float x;
+
+		public void Create(GameObject g)
+		{
+			TweenX.Add(g, 2f, x).EaseOutQuart();
+		}
+	}
+}
